3224d6dd5bc7f548ec0a580af1ba6a76c18fa83e
only805 <only805@163.com>
2016-11-15 12:55:46 +0800

    更新文件

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 (function(){
     var html=document.documentElement;
-    var width=html.getBoundingClientRect().width;   
+    var width=html.getBoundingClientRect().width; 
+    if(width>768){return width=768;}  
     html.style.fontSize=width/(640/20)+'px';   
 })();
 
